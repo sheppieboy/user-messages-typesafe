f3451014ade258938c6ef64e90e4642e3af082cf
Luke Sheppard <sheppiegit@gmail.com>
2022-11-02 17:31:59 -0400

    Added a Branch component that renders user messages in a tree format from the user display

--- src/components/MessageDisplay.tsx
+++ src/components/MessageDisplay.tsx
@@ -1,4 +1,5 @@
 import { Message } from "../types";
+import Branch from "./Branch";
 
 type Props = {
   message: Message;
@@ -8,6 +9,7 @@ type Props = {
 const MessageDisplay = ({ message, index }: Props) => {
   return (
     <div className="group mb-2 shrink-0 rounded-lg flex items-center drop-shadow-md w-48 h-20 bg-zinc-700 relative">
+      <Branch trunk={index === 0} />
       <p className="text-sm text-gray-200 font-bold px-4">{message.body}</p>
     </div>
   );
